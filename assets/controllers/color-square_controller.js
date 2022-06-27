d76f2f26c01f9197291199aa09e80f37237cf705
Toky Randriamalala <justoky@hotmail.fr>
2022-06-28 00:32:06 +0200

    selection choix unique de cards

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,7 +1,13 @@
 import { Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
+    static targets = ['colorSquare'] //toujours static pour utiliser la syntaxe
     selectColor(event) {
-        console.log(event);
+        this.colorSquareTargets.forEach((element) => {
+            element.classList.remove('selected');
+        });
+        console.log(this.colorSquareTargets);
+        event.currentTarget.classList.add('selected');
+
     }
 }
