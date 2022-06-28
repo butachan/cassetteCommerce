73be16154aa13d0021f8bfaf034ef63ecdac896c
Toky Randriamalala <justoky@hotmail.fr>
2022-06-29 00:09:50 +0200

    autocomplete search from database ajax

--- assets/controllers/search-preview_controller.js
+++ assets/controllers/search-preview_controller.js
@@ -4,7 +4,14 @@ export default class extends Controller {
     static values = {
         url: String,
     }
-    onSearchInput(event) {
-        console.log(this.urlValue);
+    static targets = ['result'];
+    async onSearchInput(event) {
+       const params = new URLSearchParams({
+            q: event.currentTarget.value,
+            preview: 1,
+        });
+        const response = await fetch(`${this.urlValue}?${params.toString()}`);
+        this.resultTarget.innerHTML = await response.text();
+        console.log(await response.text());
     }
 }
